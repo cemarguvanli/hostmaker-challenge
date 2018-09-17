c9d43b3399ca44b8bfa417d5d4209b278346f64f
cemarguvanli <cemarguvanli@gmail.com>
2018-09-17 19:08:30 +0300

    app wrapped with error boundary component

--- app/App.js
+++ app/App.js
@@ -1,13 +1,16 @@
 import React, { Fragment } from 'react';
 
 import Header from 'components/Header';
+import ErrorBoundary from 'components/ErrorBoundary';
 
 import Routes from './routes';
 
 const App = () => (
   <Fragment>
-    <Header />
-    { Routes }
+    <ErrorBoundary>
+      <Header />
+      { Routes }
+    </ErrorBoundary>
   </Fragment>
 );
 
