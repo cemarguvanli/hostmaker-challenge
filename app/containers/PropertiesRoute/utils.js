e7a4d33df79748dce8185c580246d04ac3178df0
cemarguvanli <cemarguvanli@gmail.com>
2018-09-17 18:40:47 +0300

    added async func fetch for google geocode

--- app/containers/PropertiesRoute/utils.js
+++ app/containers/PropertiesRoute/utils.js
@@ -6,26 +6,33 @@ const getAddresses = (properties) => (
   ))
 );
 
-const fetchPropertiesRoute = (data) => {
+async function fetchAsync(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+const fetchPropertiesRoute = async (data) => {
   const apiUrl = 'https://maps.google.com/maps/api/geocode/json?key=AIzaSyDNpaoitYV0_lcX-2aS8j20XAh8tcfV6Pk';
   const addresses = getAddresses(data);
 
-  const promises = addresses.map((address) => fetch(`${apiUrl}&address=${address}`).then((res) => res.json()));
+  const promises = addresses.map((address) => fetchAsync(`${apiUrl}&address=${address}`));
+  const solvedResults = await Promise.all(promises);
 
-  return Promise.all(promises).then((results) => (
-    results.map((res) => {
-      const position = res.results[0].geometry.location;
-      const address = res.results[0].formatted_address;
-      const placeId = res.results[0].place_id;
+  return solvedResults.map((res) => {
+    const { results: [item] } = res;
 
-      return {
-        showInfo: false,
-        placeId,
-        position,
-        address,
-      };
-    })
-  ));
+    const position = item.geometry.location;
+    const address = item.formatted_address;
+    const placeId = item.place_id;
+
+    return {
+      showInfo: false,
+      placeId,
+      position,
+      address,
+    };
+  });
 };
 
 export { fetchPropertiesRoute };
